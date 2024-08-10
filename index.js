cf803e38f881737c817a6d54b7c053d6742918d2
Sridharan23 <sri20032003@gmail.com>
2024-08-10 06:25:07 +0530

    changes

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ var swiper = new Swiper(".home", {
     },
    
   });
+var menu=document.getElementById("menu-icon")
+  var closenav = document.getElementById("closenav")
+  var sidenav = document.getElementById("sidenav")
+closenav.addEventListener("click",()=>{
+  sidenav.style.transform="translateX(100%)"
+})
+  menu.addEventListener("click",()=>{
+    sidenav.style.display="block"
+    sidenav.style.transform="translateX(0)"
+  })
